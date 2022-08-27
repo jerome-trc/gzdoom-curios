12656dbf2875d3a534eb50a968c50cdea395307e
j-martina <jerome.jaa.martina@gmail.com>
2022-08-26 22:21:48 -0400

    Shell script to quickly package all existing mods

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p ./out
+
+cd ./cartographer
+zip -0 -r ../out/cartographer.pk3 *
+
+cd ../chprog
+zip -0 -r ../out/colourful-hell-prog.pk3 *
+
+cd ../disk-jockey
+zip -0 -r ../out/disk-jockey.pk3 *
+
+cd ../drla-thrifty
+zip -0 -r ../out/drla-thrifty.pk3 *
+
+cd ../rat-crosshair
+zip -0 -r ../out/rat-crosshair.pk3 *
+
+cd ../rat-tools
+zip -0 -r ../out/rat-tools.pk3 *
