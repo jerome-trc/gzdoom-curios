b2717a342b57fd47c9de2daae49e7d29f01da175
jerome-trc <circus.of.rats@outlook.com>
2023-08-14 10:17:12 -0400

    Purge `chprog` and `drla-nomadic`
    I'm not interested in Colourful Hell as a monster mod and the current gimmick underpinning DRLAX's Nomad needs overhaul.

--- build.sh
+++ build.sh
@@ -5,17 +5,11 @@ mkdir -p ./out
 cd ./cartographer
 zip -0 -r ../out/cartographer.pk3 *
 
-cd ../chprog
-zip -0 -r ../out/colourful-hell-prog.pk3 *
-
 cd ../disk-jockey
 zip -0 -r ../out/disk-jockey.pk3 *
 
 cd ../drla-bonsai
 zip -0 -r ../out/drla-bonsai.pk3 *
-
-cd ../drla-nomadic
-zip -0 -r ../out/nomadic-lifestyle.pk3 *
 
 cd ../drla-thrifty
 zip -0 -r ../out/drla-thrifty.pk3 *
